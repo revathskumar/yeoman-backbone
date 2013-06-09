5fb4ba2495275363c39966145063a28c823711ed
Revath S Kumar <rsk@revathskumar.com>
2013-06-09 22:45:25 +0530

    intial commit

---
+++ app/scripts/collections/todos-collection.js
@@ -0,0 +1,5 @@
+backboneRequirejs.Collections.TodosCollection = Backbone.Collection.extend({
+
+  model: backboneRequirejs.Models.TodosModel
+
+});
---
+++ app/scripts/routes/todo-router.js
@@ -0,0 +1,16 @@
+/*global define*/
+
+define([
+    'jquery',
+    'backbone',
+], function ($, Backbone) {
+    'use strict';
+
+  var TodoRouter = Backbone.Router.extend({
+    routes: {
+    },
+
+  });
+
+  return TodoRouter;
+});
